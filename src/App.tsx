b877ebdd749b1da1591cbee7a87c7af3e03a1eb4
yuzhou <yuzhou.ini@outlook.com>
2024-07-11 16:44:23 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>双色球</div>
+      <div></div>
+      <div className="card">
+        <button>I'm Feeling Lucky</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
